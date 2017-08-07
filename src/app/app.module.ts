b383b1e7579df485afa84d967c42ec8c8e9ac0a0
zakms <mz.soufiani@gmail.com>
2017-08-07 21:05:31 +1000

    added material and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MdSidenavModule} from '@angular/material';
+import {MdInputModule} from '@angular/material';
+import {MdButtonModule} from '@angular/material';
+import {MdIconModule} from '@angular/material';
+
+
+
+import 'hammerjs';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    FlexLayoutModule,
+    MdSidenavModule,
+    MdInputModule,
+    MdButtonModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
